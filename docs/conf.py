4f38b8517297b951480a946aeb04531e950763a7
Jordan Massiah <jormas@amazon.com>
2018-08-30 16:47:55 +0100

    Mock mxnet.ndarray

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet', 'sklearn', 'sklearn.svm', 'sklearn.linear_model']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet', 'sklearn', 'sklearn.svm', 'sklearn.linear_model',
+                'mxnet.ndarray']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
