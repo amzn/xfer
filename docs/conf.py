83087956e9e0ba071ccca63ddf745e5a3b591f7f
Jordan Massiah <jormas@amazon.com>
2018-08-30 16:25:09 +0100

    Mock murmurhash3_32

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet', 'scipy.sparse']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet', 'scipy.sparse', 'murmurhash3_32']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
