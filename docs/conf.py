91315192bb5cc447a736d24e27f3515f99960423
Jordan Massiah <jormas@amazon.com>
2018-08-30 16:43:19 +0100

    Fix typo

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet', 'sklearn]
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet', 'sklearn']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
