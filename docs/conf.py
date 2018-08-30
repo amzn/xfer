5f6342446f8e44c8a7388885743ff435bb8d8706
Jordan Massiah <jormas@amazon.com>
2018-08-30 16:07:13 +0100

    Mock xfer

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet', 'xfer']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
