eb946c752ded4c041c6597302072a87b99cdbcf8
Jordan Massiah <jormas@amazon.com>
2018-08-30 16:23:09 +0100

    Mock scipy.sparse

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'GPy', 'mxnet', 'scipy.sparse']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
