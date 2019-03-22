13757c7f15b92ee0402dfcf60d028596f419917d
Jordan Massiah <jormas@amazon.com>
2019-03-22 11:21:55 +0000

    Add Gpy to req

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ requires = [
     'mxnet>=1.2.0',
     'numpy>=1.10.1',  # GPy import throws error for lower versions
     'scikit-learn>=0.19.2',
-    'matplotlib>=2.2.2'
+    'matplotlib>=2.2.2',
+    'GPy>=1.9.5'
 ]
 
 
